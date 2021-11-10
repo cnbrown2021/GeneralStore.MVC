d95077f9049feb8da832c44bdd2ce5e3bcd0cbfa
Ciara Brown <cnbrown2021@gmail.com>
2021-11-09 19:14:46 -0500

    Customer Edit

--- GeneralStore.MVC/Controllers/CustomerController.cs
+++ GeneralStore.MVC/Controllers/CustomerController.cs
@@ -69,10 +69,36 @@ namespace GeneralStore.MVC.Controllers
 
 
         // GET : Edit
+        // Customer/Edit/{id}
+        public ActionResult Edit(int? id)
+        {
+            if(id == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+            Customer customer = _db.Customers.Find(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
 
 
         // POST : Edit
-
+        // Customer/Edit/{id}
+        [HttpPost, ActionName("Edit")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Customer customer)
+        {
+            if(ModelState.IsValid)
+            {
+                _db.Entry(customer).State = System.Data.Entity.EntityState.Modified;
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(customer);
+        }
 
         // GET : Detail
     }
